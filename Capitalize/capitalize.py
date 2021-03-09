40bcdef7aa1beb7f76f3d77f5f31e5bfae5e63fa
Umutambyi Gad <umutambyig@gmail.com>
2021-03-09 17:42:55 +0100

    Used list comprehension
    Just saves some lines of code

--- Capitalize/capitalize.py
+++ Capitalize/capitalize.py
@@ -1,11 +1,6 @@
 import sys
 
 def capitalize(word):
-  word_in_list = word.split()
-
-  for index, item in enumerate(word_in_list):
-    word_in_list[index] = item.capitalize()
-
-  return " ".join(word_in_list)
+	return " ".join([i.capitalize() for i in word.split()])
 
 print(capitalize(sys.argv[1]))
