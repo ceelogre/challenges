58809a60b7c987e128df6d081f63239c1a37752e
ceelogre <sam.nishimwe@yahoo.com>
2021-03-15 21:23:04 +0200

    feat: prime digit sum untested

--- PrimeDigitSum/prime_digit_sum.py
+++ PrimeDigitSum/prime_digit_sum.py
@@ -1,28 +1,30 @@
 """
 Chloe number is a number whose three, four and five consecutive digits sum is a prime number
 """
-def first_n_digit_number(digits_number):
-  first_number = str(1)
-  for i in range(1, digits_number):
-    first_number += '0'
-  return int(first_number)
-
-def last_n_digit_number(digits_number):
-  return first_n_digit_number(1+digits_number) -1
-
 
 def is_prime(number):
   # starts from 2 up to the number
-  if number == 2 or number == 3 or number == 5 or number == 7:
+  digits_sum = 0
+  while ( number != 0):
+    digits_sum += number %10
+    number = number // 10
+
+  # one is not a prime number as it doesn't have two factors
+  if digits_sum == 1: return False 
+  if digits_sum == 2 or digits_sum == 3 or digits_sum == 5 or digits_sum == 7:
     return True
-  return number %2 != 0 and number %3 != 0 and number %5 !=0 and number %7 != 0
+  return digits_sum %2 != 0 and digits_sum %3 != 0 and digits_sum %5 !=0 and digits_sum %7 != 0
 
 def prime_digit_sum(digits):
   prime_count = 0
-  for i in range (first_n_digit_number(digits), last_n_digit_number(digits)):
+  # any number below 9999 cannot satisfy the condition as 5 digits sums are not available
+  if digits < 5: return prime_count
+
+  for i in range (10 **(digits-1), 10 **digits ):
     if consecutive_digits(i, 3) :
       if consecutive_digits(i, 4):
         if consecutive_digits(i, 5):
+          print(i)
           prime_count += 1 
   return prime_count
 
