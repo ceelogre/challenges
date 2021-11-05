b712ecdd012326c4be8d0842ce1bb869e8ec1857
ceelogre <sam.nishimwe@yahoo.com>
2021-11-05 10:45:00 +0200

    feat: smallest alt method buggy \

--- SmallestUnseen/smallestAlt.js
+++ SmallestUnseen/smallestAlt.js
@@ -1,18 +1,24 @@
 const smallestUnseenInt = (arr) => {
-  const sortedA = arr.sort((a, b) => a - b);
-  let smallest = 1
-  while(sortedA.includes(smallest)) {
-    smallest++
+  const set = new Set()
+  arr.forEach(num => {
+    set.add(num)
+  })
+  for (let i = 1; i < set.size; i ++) {
+    if (set.has(i)) {
+      continue
+    } else {
+      return i
+    }
   }
-  return smallest <= 0? 1 : smallest;
-}
+  return set.size
+  }
 
-console.info(smallestUnseenInt([1, 3, 6, 4, 1, 2])) // 5
 console.info(smallestUnseenInt([1, 2, 3])) // 4
-console.info(smallestUnseenInt([-5, -3, -1])) // 1
+console.info(smallestUnseenInt([9,9,9,9,9]))
+console.info(smallestUnseenInt([10, 1, 2, 3, 4, 4, 5])) // 6
+console.info(smallestUnseenInt([139, 394, 309,0932, 240]))
 console.info(smallestUnseenInt([10, 1, 2, 3, 4, 4, 5])) // 6
 console.info(smallestUnseenInt([1, 3, 6, 7, 9, 13])) // 2
 console.info(smallestUnseenInt([-3, -2, -1, 0, 1, 2])) // 3
 console.info(smallestUnseenInt([139, 394, 309,0932, 240])) // 1
 console.info(smallestUnseenInt([-394, -4, 3490394]))
-console.info(smallestUnseenInt([10, 1, 2, 3, 4, 4, 5])) // 6
