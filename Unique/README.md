f361ba72fdb84a206ef5768bc824f0b84f026faa
Samuel Nishimwe <sam.nishimwe@yahoo.com>
2021-11-07 13:25:05 +0200

    feat: unique (#17)

---
+++ Unique/README.md
@@ -0,0 +1,6 @@
+## First unique number 
+Find the first unique number in a given sequence.
+ The unique number is the number that occurs exactly once in array.
+ ### sample tests
+`[3,9, 19, 9, 34, 1, 3] -> 19`
+`[6, 2, 60, 49, 2, 4, 10, 3] -> 6`
---
+++ Unique/unique.py
@@ -0,0 +1,14 @@
+def first_unique(array):
+  for index, element in enumerate(array):
+    # remove the elt
+    array.pop(index)
+    if element in array:
+      pass
+    else:
+      return element
+  return 'No unique element'
+
+print(first_unique([3, 9, 19, 9, 34, 1, 3]))
+print(first_unique([6, 2, 60, 49, 2, 4, 10, 3]))
+print(first_unique([2, 2]))
+print(first_unique([]))
